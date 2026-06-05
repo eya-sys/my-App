20fa1206cb6aa38d63a8dc4dd2b96624db13b3b6
eya_khlifa <eyakhlifa535@gmail.com>
2026-06-05 16:27:48 +0100

    Build web

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"052f31d115eceda8cbff1b3481fcde4330c4ae
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1046311339" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2698201643" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
